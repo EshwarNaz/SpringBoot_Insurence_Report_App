0af0623400f341530e482f9c2123a03d54be67f2
EshwarNaz <eshwarnaz786@gmail.com>
2023-05-09 14:32:07 +0530

    Screen Developed.

--- src/main/java/in/eshwarnaz/util/ExcelGenerator.java
+++ src/main/java/in/eshwarnaz/util/ExcelGenerator.java
@@ -1,5 +1,73 @@
 package in.eshwarnaz.util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Service;
+
+import in.eshwarnaz.entity.CitizenPlan;
+
+@Service
 public class ExcelGenerator {
+
+	public void excelGenrator(HttpServletResponse responce, List<CitizenPlan> records) throws Exception {
+
+		Workbook workbook = new HSSFWorkbook();
+		Sheet sheet = workbook.createSheet();
+		Row headerRow = sheet.createRow(0);
+		headerRow.createCell(0).setCellValue("Id");
+		headerRow.createCell(1).setCellValue("Citizen Name");
+		headerRow.createCell(2).setCellValue("Plan Name");
+		headerRow.createCell(3).setCellValue("Gender");
+		headerRow.createCell(4).setCellValue("Plan Status");
+		headerRow.createCell(5).setCellValue("Plan Start Date");
+		headerRow.createCell(6).setCellValue("Plan End Date");
+		headerRow.createCell(7).setCellValue("Benfit Amount");
+
+		int dataRowIndex = 1;
+		for (CitizenPlan plan : records) {
+			Row dataRow = sheet.createRow(dataRowIndex);
+			dataRow.createCell(0).setCellValue(plan.getCITEGEN_ID());
+			dataRow.createCell(1).setCellValue(plan.getCITEGEN_NAME());
+			dataRow.createCell(2).setCellValue(plan.getCITEGENPLANNAME());
+			dataRow.createCell(3).setCellValue(plan.getCITEGEN_GENDER());
+			dataRow.createCell(4).setCellValue(plan.getCITEGENPLANSTATUS());
+			if (null != plan.getBENFITAMOUNT()) {
+				dataRow.createCell(5).setCellValue(plan.getSTART_DATE() + "");
+			} else {
+				dataRow.createCell(5).setCellValue("N/A");
+			}
+			if (null != plan.getBENFITAMOUNT()) {
+				dataRow.createCell(6).setCellValue(plan.getEND_DATE() + "");
+			} else {
+				dataRow.createCell(6).setCellValue("N/A");
+			}
+
+			if (null != plan.getBENFITAMOUNT()) {
+				dataRow.createCell(7).setCellValue(plan.getBENFITAMOUNT());
+			} else {
+				dataRow.createCell(7).setCellValue("N/A");
+			}
+
+			dataRowIndex++;
+		}
+
+		FileOutputStream fos = new FileOutputStream(new File("plans.xls"));
+		workbook.write(fos);
+		fos.close();
+
+		ServletOutputStream outputStream = responce.getOutputStream();
+		workbook.write(outputStream);
+		workbook.close();
+
+	}
 
 }
--- src/main/java/in/eshwarnaz/util/PdfGenerator.java
+++ src/main/java/in/eshwarnaz/util/PdfGenerator.java
@@ -1,5 +1,63 @@
 package in.eshwarnaz.util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Service;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+import in.eshwarnaz.entity.CitizenPlan;
+
+@Service
 public class PdfGenerator {
+
+	public void pdfGenerator(HttpServletResponse responce, List<CitizenPlan> records, File f)
+			throws DocumentException, IOException {
+
+		Document document = new Document(PageSize.A4);
+		PdfWriter.getInstance(document, responce.getOutputStream());
+		PdfWriter.getInstance(document, new FileOutputStream(f));
+		document.open();
+		Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLDOBLIQUE);
+		Paragraph p = new Paragraph("Citizen Plan Info", font);
+		p.setAlignment(p.ALIGN_CENTER);
+		document.add(p);
+		PdfPTable table = new PdfPTable(8);
+		table.setWidthPercentage(100f);
+		table.setSpacingBefore(5);
+		table.addCell("Id");
+		table.addCell("Citizen Name");
+		table.addCell("Plan Name");
+		table.addCell("Gender");
+		table.addCell("Plan Status");
+		table.addCell("Plan StartDate");
+		table.addCell("Plan EndDate");
+		table.addCell("Benfit Amount");
+
+		for (CitizenPlan plan : records) {
+			table.addCell(String.valueOf(plan.getCITEGEN_ID()));
+			table.addCell(plan.getCITEGEN_NAME());
+			table.addCell(plan.getCITEGENPLANNAME());
+			table.addCell(plan.getCITEGEN_GENDER());
+			table.addCell(plan.getCITEGENPLANSTATUS());
+			table.addCell(plan.getSTART_DATE() + "");
+			table.addCell(plan.getEND_DATE() + "");
+			table.addCell(plan.getBENFITAMOUNT() + "");
+		}
+		document.add(table);
+		document.close();
+	}
 
 }
